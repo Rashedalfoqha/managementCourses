bf9edbdf2258cc70fd577cf29bd553121a6311e4
Rashedalfoqha <rashedmohammadalfoqha@gmail.com>
2024-06-08 19:04:01 +0300

    DB connectd

---
+++ BackEnd/models/db.js
@@ -0,0 +1,14 @@
+const { Pool } = require("pg");
+const connectionString = process.env.DB_URL;
+const pool = new Pool({
+  connectionString
+});
+pool
+  .connect()
+  .then((res) => {
+    console.log(`DB connected to ${res.database}`);
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+module.exports = { pool };
