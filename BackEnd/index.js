1b29bee5eb74f9ff71e4295386b04b9593adf083
Rashedalfoqha <rashedmohammadalfoqha@gmail.com>
2024-06-08 20:24:20 +0300

    build router and checktest function

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -6,7 +6,9 @@ app.use(express.json());
 const PORT = process.env.PORT || 5000;
 const cors = require("cors");
 app.use(cors());
+const usersRouter = require("./routes/users");
 
+app.use("/users", usersRouter);
 app.listen(PORT, () => {
   console.log(`connect server localHost ${PORT}`);
 });
---
+++ BackEnd/routes/users.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const { register, login } = require("../controller/users");
+const usersRouter = express.Router();
+usersRouter.post("/register", register);
+usersRouter.post("/login", login);
+
+module.exports = usersRouter;
