b974c54acbfd8cd71517403495da7ff4597376da
Rashedalfoqha <rashedmohammadalfoqha@gmail.com>
2024-06-09 01:47:02 +0300

    courses getAllByid and get all courses is done

---
+++ BackEnd/controller/courses.js
@@ -0,0 +1,97 @@
+const { pool } = require("../models/db");
+const createCourses = (req, res) => {
+  const { photo, video, title, description } = req.body;
+  const user_id = req.token.userId;
+  const query = `INSERT INTO courses (photo, video, title, description,user_id)
+    VALUES ($1, $2, $3, $4,$5)
+    RETURNING *;`;
+  const value = [photo, video, title, description, user_id];
+  pool.query(query, value).then((result) => {
+    res
+      .status(201)
+      .json({
+        message: "created new course successfully",
+        result: result.rows[0]
+      })
+      .catch((err) => {
+        res.status(500).json({
+          success: false,
+          message: "Server error",
+          error: err.message
+        });
+      });
+  });
+};
+const getAllCoursesForUser = (req, res) => {
+  const query = `SELECT 
+  courses.*,
+  users.firstName, 
+  users.lastName,
+  users.email,
+  users.age,
+  users.country
+FROM 
+  courses
+INNER JOIN 
+  users ON courses.user_id = users.id
+WHERE 
+  courses.is_deleted = 0
+ORDER BY 
+  courses.created_at;`;
+  pool
+    .query(query)
+    .then((result) => {
+      res.status(200).json({
+        message: "all courses",
+        result: result.rows
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: "Server error",
+        error: err.message
+      });
+    });
+};
+const getAllCourses = (req, res) => {
+  const query = `SELECT courses.*,
+  users.firstName, 
+  users.lastName,
+  users.email,
+  users.age,
+  users.country
+  FROM courses, users
+  WHERE courses.is_deleted = 0
+  ORDER BY courses.created_at;`;
+  pool
+    .query(query)
+    .then((result) => {
+      res.status(200).json({
+        message: "all courses",
+        result: result.rows
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: "Server error",
+        error: err.message
+      });
+    });
+};
+const updateCourses = (req, res) => {
+  const { photo, video, title, description } = req.body;
+  const query = ` UPDATE courses 
+  SET 
+    photo = COALESCE($1, photo),
+    video = COALESCE($2, video),
+    title = COALESCE($3, title),
+    description = COALESCE($4, description),
+  WHERE 
+    id = $5 
+    AND is_deleted = 0 
+  RETURNING *`;
+  const value = [photo, video, title, description];
+};
+module.exports = { getAllCoursesForUser, getAllCourses, createCourses };
--- BackEnd/routes/courses.js
+++ BackEnd/routes/courses.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const coursesRouter = express.Router();
-const { createCourses } = require("../controller/coruses");
+const {
+  createCourses,
+  getAllCoursesForUser,
+  getAllCourses
+} = require("../controller/courses");
 const authentication = require("../middleware/authentication");
 const authorization = require("../middleware/authorization");
-coursesRouter.post("/create",authentication,authorization("CREATE_COURSES"),createCourses)
+coursesRouter.post(
+  "/create",
+  authentication,
+  authorization("CREATE_COURSES"),
+  createCourses
+);
+coursesRouter.get("/", getAllCoursesForUser);
+coursesRouter.get("/all", getAllCourses);
 module.exports = coursesRouter;
