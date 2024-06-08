e26f5c0ed481a99345785bfddbb9dfaf4fe74bad
Rashedalfoqha <rashedmohammadalfoqha@gmail.com>
2024-06-08 19:37:37 +0300

    added fav tables and fuction createTable

--- BackEnd/models/db.js
+++ BackEnd/models/db.js
@@ -11,4 +11,67 @@ pool
   .catch((err) => {
     console.log(err.message);
   });
+const createTable = () => {
+  pool
+    .query(
+      `CREATE TABLE roles (
+        id SERIAL PRIMARY KEY,
+        role VARCHAR(255) NOT NULL
+    );
+    CREATE TABLE permissions (
+        id SERIAL PRIMARY KEY,
+        permission VARCHAR(255) NOT NULL
+    );
+    CREATE TABLE role_permission (
+        id SERIAL PRIMARY KEY,
+        role_id INT REFERENCES roles(id),
+        permission_id INT REFERENCES permissions(id)
+    );
+    CREATE TABLE students (
+        id SERIAL PRIMARY KEY,
+        photo VARCHAR,
+        cover VARCHAR,
+        firstName VARCHAR(255),
+        lastName VARCHAR(255),
+        email VARCHAR(255) UNIQUE NOT NULL,
+        age INT,
+        country VARCHAR(255),
+        password VARCHAR(255),
+        courses INT,
+        role_id INT REFERENCES roles(id),
+        is_deleted SMALLINT DEFAULT 0
+    );
+    CREATE TABLE courses (
+        id SERIAL PRIMARY KEY,
+        photo VARCHAR,
+        video VARCHAR,
+        title VARCHAR,
+        teachers_id INT REFERENCES teachers(id),
+        description VARCHAR NOT NULL,
+        is_deleted SMALLINT DEFAULT 0,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+    CREATE TABLE teachers (
+        id SERIAL PRIMARY KEY,
+        photo VARCHAR,
+        cover VARCHAR,
+        firstName VARCHAR(255),
+        lastName VARCHAR(255),
+        email VARCHAR(255) UNIQUE NOT NULL,
+        age INT,
+        country VARCHAR(255),
+        password VARCHAR(255),
+        courses INT,
+        role_id INT REFERENCES roles(id),
+        is_deleted SMALLINT DEFAULT 0
+    );
+    CREATE TABLE student_favorites (
+        id SERIAL PRIMARY KEY,
+        student_id INT REFERENCES students(id),
+        course_id INT REFERENCES courses(id)
+    );`
+    )
+    .then((result) => {})
+    .catch(() => {});
+};
 module.exports = { pool };
